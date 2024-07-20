d0ae1372dc961c2e940dfd0d15b79b1596b02b85
stag7824 <atalha54@gmail.com>
2024-07-20 10:16:54 +0500

    improved bg

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -9,16 +9,27 @@ function App() {
 
         <div className='flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl'>
         
-          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "red" }} onClick={() => setBgColor("red")}>RED</button>
-
-          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Green" }} onClick={() => setBgColor("green")}>GREEN</button>
-
-          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Blue" }} onClick={() => setBgColor("blue")}>BLUE</button>
-
-          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Cyan" }} onClick={() => setBgColor("cyan")}>CYAN</button>
-
-          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Grey" }} onClick={() => setBgColor("grey")}>GREY</button>
-
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "red", color:"white" }} onClick={() => setBgColor("red")}>RED</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Green", color:"white" }} onClick={() => setBgColor("green")}>GREEN</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Blue", color:"white" }} onClick={() => setBgColor("blue")}>BLUE</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Cyan", color:"white" }} onClick={() => setBgColor("cyan")}>CYAN</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Grey", color:"white" }} onClick={() => setBgColor("grey")}>GREY</button>
+          {/* Provide all the reamining color pellets */}
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Yellow", color:"white" }} onClick={() => setBgColor("yellow")}>YELLOW</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Purple", color:"white" }} onClick={() => setBgColor("purple")}>PURPLE</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Pink", color:"white" }} onClick={() => setBgColor("pink")}>PINK</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Orange", color:"white" }} onClick={() => setBgColor("orange")}>ORANGE</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Brown", color:"white" }} onClick={() => setBgColor("brown")}>BROWN</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Black", color:"white" }} onClick={() => setBgColor("black")}>BLACK</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "White", color:"black" }} onClick={() => setBgColor("white")}>WHITE</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Gray", color:"white" }} onClick={() => setBgColor("gray")}>GRAY</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Silver", color:"black" }} onClick={() => setBgColor("silver")}>SILVER</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Gold", color:"black" }} onClick={() => setBgColor("gold")}>GOLD</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Navy", color:"white" }} onClick={() => setBgColor("navy")}>NAVY</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Teal", color:"white" }} onClick={() => setBgColor("teal")}>TEAL</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Lime", color:"black" }} onClick={() => setBgColor("lime")}>LIME</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Aqua", color:"black" }} onClick={() => setBgColor("aqua")}>AQUA</button>
+          <button className='outline-none px-4 rounded-full shadow-lg' style={{ backgroundColor: "Maroon", color:"white" }} onClick={() => setBgColor("maroon")}>MAROON</button>
         </div>
       </div>
     </div>
